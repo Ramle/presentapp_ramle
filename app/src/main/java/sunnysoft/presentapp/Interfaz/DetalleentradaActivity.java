89c76c823761787267e0dcd9620a9a57e5733e11
Ramle <ramle_12@hotmail.com>
2018-02-25 22:20:24 -0500

    detalle entrada

--- app/src/main/java/sunnysoft/presentapp/Interfaz/DetalleentradaActivity.java
+++ app/src/main/java/sunnysoft/presentapp/Interfaz/DetalleentradaActivity.java
@@ -53,6 +53,7 @@ import sunnysoft.presentapp.Datos.DatabaseHelper;
 import sunnysoft.presentapp.Interfaz.adapter.FieldsEventoAdapter;
 import sunnysoft.presentapp.Interfaz.pojo.FieldsEvento;
 import sunnysoft.presentapp.R;
+import android.support.v7.widget.Toolbar;
 
 public class DetalleentradaActivity extends AppCompatActivity {
 
@@ -68,10 +69,10 @@ public class DetalleentradaActivity extends AppCompatActivity {
     TextView dataresponsables;
     ImageView img_persona;
     String email;
-    private TabLayout tabLayout;
     final ProgressDialog[] progressDialog = new ProgressDialog[1];
     List<String>nomesImage = new ArrayList<>();
     int notification_count;
+    private Toolbar secundaria;
 
     String token;
     String subdomain;
@@ -91,14 +92,12 @@ public class DetalleentradaActivity extends AppCompatActivity {
 
         Bundle datos = getIntent().getExtras();
         url = datos.getString("DetailUrl");
-        urlT = datos.getString("tURL");
         midb = new DatabaseHelper(this);
 
 
 
         Cursor Resultados = midb.Session();
 
-        seteartabs(urlT);
 
         token =Resultados.getString(Resultados.getColumnIndex("token"));
         email =Resultados.getString(Resultados.getColumnIndex("user"));
@@ -281,6 +280,26 @@ public class DetalleentradaActivity extends AppCompatActivity {
         });
 
 
+        //Tooblar
+
+        secundaria = (Toolbar) findViewById(R.id.toolbar_secundaria);
+        secundaria.setNavigationIcon(R.drawable.arrow_back);
+        TextView titulo_secundaria = (TextView) secundaria.findViewById(R.id.toolbar_secundaria_title);
+        titulo_secundaria.setText("Ver Entrada");
+        secundaria.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(DetalleentradaActivity.this, ModulosActivity.class);
+                startActivity(i);
+            }
+        });
+
+
+        /*Toolbar toolbarfecha = (Toolbar) findViewById(R.id.toolbarfecha);
+        TextView toolbar_titlefecha = (TextView)toolbarfecha.findViewById(R.id.toolbar_titlefecha);
+        setSupportActionBar(toolbarfecha);
+        toolbar_titlefecha.setText("Ver Entrada");
+        getSupportActionBar().setDisplayShowTitleEnabled(false);*/
 
     }
 
@@ -351,225 +370,5 @@ public class DetalleentradaActivity extends AppCompatActivity {
     public List<FieldsEvento> getFieldEventos() {
         return new ArrayList<>(fieldsEventos.values());
     }
-
-    /////////////////////llenar tabs ///////////////////////////////////////////
-    public void seteartabs(String url){
-
-        AsyncHttpClient client = new AsyncHttpClient();
-        JSONObject jsonParams = new JSONObject();
-        StringEntity entity = null;
-
-        tabLayout = (TabLayout) findViewById(R.id.tabsent);
-        tabLayout.setTabMode(TabLayout.MODE_SCROLLABLE);
-
-
-        // llamado del servicio
-        RequestHandle post  = client.get(url, new AsyncHttpResponseHandler() {
-
-
-            @Override
-            public void onStart(){
-
-                super.onStart();
-                /*progressDialog[1] = ProgressDialog.show(
-                        getApplicationContext(), "Por favor espere", "Procesando...");*/
-
-            }
-
-            @Override
-            public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
-
-                // Declaracion de variables
-                String responseStr = null;
-
-                //respuesta del servicio
-                try {
-                    responseStr = new String(responseBody, "UTF-8");
-                    // manejo del primer nivel de objetos
-
-                    JSONArray items = new JSONArray(responseStr);
-
-                    for(int i=0; i < items.length(); i++) {
-
-                        String item = items.getString(i);
-
-                        JSONObject valores = new JSONObject(item);
-
-                        String image_user = valores.getString("image");
-                        notification_count = valores.getInt("notification_count");
-                        nomesImage.add(image_user);
-
-                    }
-
-                    for (int a = 0; a< nomesImage.size(); a++){
-                        /////////////////////creacion de tabs dinamicamente//////////////////////
-                        new DownloadImageTab().execute(nomesImage.get(a));
-                    }
-                    tabLayout.setScrollPosition(2,0f,true);
-
-
-                    tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
-                        @Override
-                        public void onTabSelected(TabLayout.Tab tab) {
-
-                        }
-
-                        @Override
-                        public void onTabUnselected(TabLayout.Tab tab) {
-                            Intent i = new Intent(getApplicationContext(),EntradasActivity.class);
-                            i.putExtra("posicion",tab.getPosition());
-                            startActivity(i);
-                        }
-
-                        @Override
-                        public void onTabReselected(TabLayout.Tab tab) {
-
-                        }
-                    });
-
-                    //progressDialog[0].dismiss();
-
-                } catch (UnsupportedEncodingException e) {
-                    e.printStackTrace();
-                } catch (JSONException e) {
-                    e.printStackTrace();
-                }
-
-            }
-
-            @Override
-            public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error) {
-
-                if (statusCode == 421) {
-                    //declaracion de variables
-                    String responseStr = null;
-
-                    try {
-                        // respuesta del servicio
-                        responseStr = new String(responseBody, "UTF-8");
-                        // manejo de primer nivel de objetos del json
-                        JSONObject errorx = new JSONObject(responseStr);
-                        // se obtiene los valores del json
-                        String valorLlave = errorx.getString("errors");
-                        // manejo del segundo nivel de valores de json
-                        JSONObject errorxa = new JSONObject(valorLlave);
-                        // se obtiene los valores que contiene el objeto
-                        String msgerror = errorxa.getString("login");
-                        // se maneja el array json
-                        JSONArray jsonarray = new JSONArray(msgerror);
-
-                        //se obtiene cada uno de los mensajes que se encuentran dentro del json
-                        for(int i=0; i < jsonarray.length(); i++) {
-                            String mensaje = jsonarray.getString(i);
-                            Toast.makeText(DetalleentradaActivity.this, mensaje, Toast.LENGTH_LONG).show();
-                        }
-
-                        midb.logouth();
-                        midb.oncreateusers();
-                        Intent i = new Intent(DetalleentradaActivity.this, InicioActivity.class);
-                        startActivity(i);
-
-                    } catch (UnsupportedEncodingException e) {
-                        e.printStackTrace();
-                    } catch (JSONException e) {
-                        e.printStackTrace();
-                    }
-                }
-                // When Http response code is '500'
-                else if (statusCode == 500) {
-
-                    Toast.makeText(DetalleentradaActivity.this, "Erros Statuscode = 500", Toast.LENGTH_LONG).show();
-                }
-                // When Http response code other than 404, 500
-                else {
-                    Log.i("On Failure", "NN");
-                    Toast.makeText(DetalleentradaActivity.this, "On Failure ", Toast.LENGTH_LONG).show();
-
-                    //Institución no valida.
-                }
-
-            }
-
-        });
-
-    }
-
-    public class DownloadImageTab extends AsyncTask<String, Integer, Drawable> {
-        @Override
-        protected Drawable doInBackground(String... arg0) {
-            // This is done in a background thread
-            return downloadImage(arg0[0]);
-        }
-
-        protected void onPostExecute(Drawable image)
-        {
-            setImageTab(image);
-        }
-
-        private Drawable downloadImage(String _url)
-        {
-            //Prepare to download image
-            URL url;
-            BufferedOutputStream out;
-            InputStream in;
-            BufferedInputStream buf;
-
-            //BufferedInputStream buf;
-            try {
-                url = new URL(_url);
-                in = url.openStream();
-
-                // Read the inputstream
-                buf = new BufferedInputStream(in);
-
-                // Convert the BufferedInputStream to a Bitmap
-                Bitmap bMap = BitmapFactory.decodeStream(buf);
-
-                //Drawable d = new BitmapDrawable(getResources(), bMap);
-
-
-                if (in != null) {
-                    in.close();
-                }
-                if (buf != null) {
-                    buf.close();
-                }
-
-                return new BitmapDrawable(bMap);
-
-            } catch (Exception e) {
-                Log.e("Error reading file", e.toString());
-            }
-
-            return null;
-        }
-    }
-
-    private void setImageTab(Drawable drawable) {
-
-        setBackgroundDrawableTab(drawable);
-    }
-
-    public void setBackgroundDrawableTab(Drawable drawable) {
-
-        //logocol.setBackgroundDrawable(drawable);
-
-        TabLayout.Tab t = tabLayout.newTab();
-        t.setCustomView(R.layout.item_tabs);
-        ImageView image = (ImageView)t.getCustomView().findViewById(R.id.img_persona);
-        CardView card = (CardView)t.getCustomView().findViewById(R.id.card_notificacion);
-
-        if (notification_count >= 0)  {
-            card.setVisibility(View.VISIBLE);
-        }else{
-            card.setVisibility(View.GONE);
-        }
-        image.setImageDrawable(drawable);
-        tabLayout.addTab(t);
-
-        //tabLayout.addTab(tabLayout.newTab().setIcon(drawable));
-    }
-
-
 
 }
